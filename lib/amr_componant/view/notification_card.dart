07a758e0b6efec9424170e26889375f47cccce92
Amr Ramadan <101355785+Atevaz@users.noreply.github.com>
2022-07-19 20:09:31 +0200

    i do notification card and circular progress card (#3)

--- lib/amr_componant/view/notification_card.dart
+++ lib/amr_componant/view/notification_card.dart
@@ -7,38 +7,79 @@ class NotificationCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        headOfNotification(),
-        const Text(' Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'),
+        bodyOfNotificationCard(),
       ],
     );
   }
 
-  Widget headOfNotification() => Row(
-    children: [
-      nameAndPhoto(),
-      dateAndTimeAndStar(),
-    ],
-  );
-
-  Widget nameAndPhoto() => Column(
-    children: [
-      Row(
-        children: const [
-          CircleAvatar(
-            radius: 50,
-            backgroundImage: AssetImage(''),
+  Widget bodyOfNotificationCard() => Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                headOfNotification(),
+                const SizedBox(
+                  height: 10,
+                ),
+                bodyTextOfNotifications(),
+              ],
+            ),
           ),
-          Text('M/Ahmed Saad'),
-        ],
-      )
-    ],
-  );
-  Widget dateAndTimeAndStar() => Column(
-    children: [
-      Text('12/2/20200'),
-      Text('08:30'),
-      Icon( Icons.stars_sharp )
-    ],
-  );
-}
+        ),
+      );
 
+  Widget headOfNotification() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          nameAndPhoto(),
+          dateAndTimeAndStar(),
+        ],
+      );
+
+  Widget nameAndPhoto() => Expanded(
+        flex: 4,
+        child: Column(
+          children: [
+            Row(
+              children: const [
+                CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage('assets/images/amr.jpg'),
+                ),
+                Expanded(
+                    child: Text(
+                  'M/Amr Ramadan',
+                  textAlign: TextAlign.center,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold
+                      ),
+                )),
+              ],
+            )
+          ],
+        ),
+      );
+
+  Widget dateAndTimeAndStar() => Expanded(
+        flex: 2,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: const [
+            Text('12/2/20200',overflow: TextOverflow.ellipsis,maxLines: 1,),
+            Text('08:30',overflow: TextOverflow.ellipsis,maxLines: 1,),
+            Icon(Icons.stars_sharp)
+          ],
+        ),
+      );
+
+  Widget bodyTextOfNotifications() => const Text(
+        'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        textAlign: TextAlign.center,
+        overflow: TextOverflow.ellipsis,
+        maxLines: 3,
+      );
+}
--- lib/component.dart
+++ lib/component.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/yassin/custom_notification_row.dart';
 import 'package:flutter_application_1/yassin/custom_text_field.dart';
 
+import 'amr_componant/view/progress_card.dart';
+
 class Component extends StatelessWidget {
   const Component({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Scaffold(body: Center(child: CustomNotificationRow())),
+    return  SafeArea(
+      child: Scaffold(
+        body: Container(),
+      ),
+
     );
   }
 }
