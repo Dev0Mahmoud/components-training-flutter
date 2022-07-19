b1c7136f2c3db3efdd598cdbebbf1abb11877382
Mahmoud Khalil <83624974+M0o0dy@users.noreply.github.com>
2022-07-19 20:10:04 +0200

    scrollbar (#2)

---
+++ lib/mahmoud/my_components.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class Component extends StatelessWidget {
+  Component({Key? key}) : super(key: key);
+
+  ScrollController _scrollController = ScrollController();
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: SafeArea(
+        child: Theme(
+          data: Theme.of(context).copyWith(
+            scrollbarTheme: ScrollbarThemeData(
+              mainAxisMargin: 20,
+              crossAxisMargin: 10,
+              thickness: MaterialStateProperty.all(10),
+              thumbColor: MaterialStateProperty.all(
+                Colors.red,
+              ),
+              trackBorderColor: MaterialStateProperty.all(
+                Colors.grey,
+              ),
+              trackColor: MaterialStateProperty.all(
+                Colors.grey,
+              ),
+
+            ),
+          ),
+          child: Scrollbar(
+            controller: _scrollController,
+            interactive: true,
+            thumbVisibility: true,
+            trackVisibility: true,
+            thickness: 10,
+            radius: const Radius.circular(20),
+            child:  SingleChildScrollView(
+              controller: _scrollController,
+              child: Text(
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+                      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+                      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+                      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+                      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+                      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+                      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+                      " when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+                      " when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+                      " when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+                      " when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+                      " when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+                      " when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+                      " when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+                      "It has survived not only five centuries, but also the leap into electronic typesetting,"
+                      " remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,"
+                      " and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+                      " when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+                      "It has survived not only five centuries, but also the leap into electronic typesetting,"
+                      " remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,"
+                      " and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+                      " when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+                      "It has survived not only five centuries, but also the leap into electronic typesetting,"
+                      " remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,"
+                      " and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
+
       designSize: const Size(414, 896),
       builder: ((context, child) {
         return MaterialApp(
@@ -26,5 +27,6 @@ class MyApp extends StatelessWidget {
         );
       }),
     );
+
   }
 }
