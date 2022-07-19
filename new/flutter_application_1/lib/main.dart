46af1ff22baaa2b1b49602782d51c236d0ee5430
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-07-19 17:23:06 +0200

    screen util and folder structure

---
+++ new/flutter_application_1/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/component.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async {
+  await ScreenUtil.ensureScreenSize();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(414, 896),
+        builder: ((context, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            builder: (context, child) {
+              ScreenUtil.init(context);
+              return Theme(
+                  data: ThemeData(
+                    primarySwatch: Colors.blue,
+                  ),
+                  child: const Component());
+            },
+          );
+        }));
+  }
+}
