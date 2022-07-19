f3477f4bad9806f15e7bcc590efc04922e21697a
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-07-19 19:32:58 +0200

    notification container

--- lib/component.dart
+++ lib/component.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/yassin/custom_notification_row.dart';
 import 'package:flutter_application_1/yassin/custom_text_field.dart';
 
 class Component extends StatelessWidget {
@@ -7,7 +8,7 @@ class Component extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const SafeArea(
-      child: Scaffold(body: Center(child: CustomTextField())),
+      child: Scaffold(body: Center(child: CustomNotificationRow())),
     );
   }
 }
