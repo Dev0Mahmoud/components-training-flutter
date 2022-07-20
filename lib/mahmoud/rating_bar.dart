fb42fad2d646d37dff4e1db7309120400dc7d4b1
Mahmoud Khalil <83624974+M0o0dy@users.noreply.github.com>
2022-07-20 17:43:26 +0200

    Mahmoud (#5)
    * test
    
    * test

---
+++ lib/mahmoud/rating_bar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+//
+// Widget myRatingBar()=> RatingBar(
+//   initialRating: 3,
+//   minRating: 1,
+//   direction: Axis.horizontal,
+//   allowHalfRating: true,
+//   itemCount: 5,
+//   itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+//   itemBuilder: (context, _) => Icon(
+//     Icons.star,
+//     color: Colors.amber,
+//   ),
+//   onRatingUpdate: (rating) {
+//     print(rating);
+//   },
+// );
